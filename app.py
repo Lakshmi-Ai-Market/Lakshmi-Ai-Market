31e13addd131e9e8784e8e56f9998a0029c4bdf8
Lakshmi-Ai-Market <kmonjit12.indiamart@gmail.com>
2025-08-26 14:06:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2457,7 +2457,7 @@ import os
 
 api_key = os.environ.get("OPENROUTER_API_KEY")
 
+# ------------------ RUN ------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
  
